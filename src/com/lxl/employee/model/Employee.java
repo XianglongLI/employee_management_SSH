3e7c876d1eabde7b77e3994e65896fe7460f740f
mike3022 <625353945@qq.com>
2017-05-27 13:44:49 +0200

    model classes and database connection

---
+++ src/com/lxl/employee/model/Employee.java
@@ -0,0 +1,57 @@
+package com.lxl.employee.model;
+
+import java.util.Date;
+
+import javax.persistence.*;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Date joinTime;
+	@ManyToOne
+	private Department department;
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Date getJoinTime() {
+		return joinTime;
+	}
+
+	public void setJoinTime(Date joinTime) {
+		this.joinTime = joinTime;
+	}
+}
